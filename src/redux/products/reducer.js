c6c768105da42a905324045e8b33da0330f299b3
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-12-31 19:16:33 +0600

    initial state removed

--- src/redux/products/reducer.js
+++ src/redux/products/reducer.js
@@ -1,22 +1,22 @@
 import { ADDPRODUCT } from "./actionTypes";
 
 const initialState = [
-  {
-    productId: 0,
-    productName: "Spring and summershoes",
-    productImage: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
-    productCategory: "Mens shoes",
-    productPrice: 400,
-    totalQty: 5,
-  },
-  {
-    productId: 1,
-    productName: "Spring and summershoes 1",
-    productImage: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
-    productCategory: "Mens shoes 1",
-    productPrice: 500,
-    totalQty: 2,
-  },
+  // {
+  //   productId: 0,
+  //   productName: "Spring and summershoes",
+  //   productImage: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
+  //   productCategory: "Mens shoes",
+  //   productPrice: 400,
+  //   totalQty: 5,
+  // },
+  // {
+  //   productId: 1,
+  //   productName: "Spring and summershoes 1",
+  //   productImage: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
+  //   productCategory: "Mens shoes 1",
+  //   productPrice: 500,
+  //   totalQty: 2,
+  // },
 ];
 
 const reducer = (state = initialState, action) => {
